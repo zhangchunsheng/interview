27727cb9f2535ccf0eb3dd2bc4accd41bab62eb5
zhangchunsheng <zhangchunsheng423@gmail.com>
2017-10-20 01:04:21 +0800

    add isclose

--- algorithm/isclose.php
+++ algorithm/isclose.php
@@ -2,3 +2,32 @@
 // 判断括号是否闭合
 // (a+b) + ((a+b))
 // ())(
+
+$str = "(a+b) + ((a+b))";
+$str = "())(";
+echo isClose($str);
+
+function isClose($str) {
+    $left = 0;
+    $right = 0;
+    for($i = 0 ; $i < strlen($str) ; $i++) {
+        $ch = $str[$i];
+        switch($ch)    {
+            case '(':
+                $left++;
+                break;
+            case ')':
+                $right = 1;
+            default:
+                if($right == 1) {
+                    if($left <= 0) {
+                        return 0;
+                    }
+                    $right = 0;
+                    $left--;
+                }
+        }
+    }
+
+    return 1;
+}
