9f8c27fdf1366b28602e69fccff755a4deb0268a
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-28 21:44:43 +0800

    add CanMakePaliQueries

---
+++ java/demo/src/main/java/com/luomor/demo/leetcode/CanMakePaliQueries.java
@@ -0,0 +1,92 @@
+package com.luomor.demo.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CanMakePaliQueries {
+    /**
+     * 给你一个字符串 s，请你对 s 的子串进行检测。
+     *
+     * 每次检测，待检子串都可以表示为 queries[i] = [left, right, k]。我们可以 重新排列 子串 s[left], ..., s[right]，并从中选择 最多 k 项替换成任何小写英文字母。
+     *
+     * 如果在上述检测过程中，子串可以变成回文形式的字符串，那么检测结果为 true，否则结果为 false。
+     *
+     * 返回答案数组 answer[]，其中 answer[i] 是第 i 个待检子串 queries[i] 的检测结果。
+     *
+     * 注意：在替换时，子串中的每个字母都必须作为 独立的 项进行计数，也就是说，如果 s[left..right] = "aaa" 且 k = 2，我们只能替换其中的两个字母。（另外，任何检测都不会修改原始字符串 s，可以认为每次检测都是独立的）
+     *
+     *
+     *
+     * 示例：
+     *
+     * 输入：s = "abcda", queries = [[3,3,0],[1,2,0],[0,3,1],[0,3,2],[0,4,1]]
+     * 输出：[true,false,false,true,true]
+     * 解释：
+     * queries[0] : 子串 = "d"，回文。
+     * queries[1] : 子串 = "bc"，不是回文。
+     * queries[2] : 子串 = "abcd"，只替换 1 个字符是变不成回文串的。
+     * queries[3] : 子串 = "abcd"，可以变成回文的 "abba"。 也可以变成 "baab"，先重新排序变成 "bacd"，然后把 "cd" 替换为 "ab"。
+     * queries[4] : 子串 = "abcda"，可以变成回文的 "abcba"。
+     *
+     *
+     * 提示：
+     *
+     * 1 <= s.length, queries.length <= 10^5
+     * 0 <= queries[i][0] <= queries[i][1] < s.length
+     * 0 <= queries[i][2] <= s.length
+     * s 中只有小写英文字母
+     * @param s
+     * @param queries
+     * @return
+     */
+    public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
+
+        List<Boolean> list = new ArrayList();
+
+        char[] s_arr = s.toCharArray();
+        //记录前n个字符个数统计
+        int[][] dic = new int[s.length()][26];
+        for (int i = 0; i < s_arr.length; i++) {
+            if (i > 0) {
+                dic[i] = dic[i - 1].clone();
+            }
+            dic[i][s_arr[i] - 'a']++;
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            //判断需要几个替换位置
+            int need_k = getnum(dic, queries[i][0], queries[i][1]);
+
+            //替换位置是否足够
+            if (need_k <= queries[i][2]) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+
+        return list;
+
+    }
+
+    public int getnum(int[][] dic, int start, int end) {
+
+        int res = 0;
+        if (start == 0) {
+            for (int i = 0; i < 26; i++) {
+                if ((dic[end][i]) % 2 != 0) {
+                    res++;
+                }
+            }
+        } else {
+            for (int i = 0; i < 26; i++) {
+                if ((dic[end][i] - dic[start - 1][i]) % 2 != 0) {
+                    res++;
+                }
+            }
+        }
+
+
+        return res / 2;
+    }
+}
